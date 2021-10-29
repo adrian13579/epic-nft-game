9c5fcd90c6b3a0d5f2c012766b48453c8106ae8d
Adrian Portales <adrianportales135@gmail.com>
2021-10-29 12:38:30 -0400

    feat: check if user has an NFT

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import twitterLogo from './assets/twitter-logo.svg';
 import SelectCharacter from './Components/SelectCharacter';
 import './App.css';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import MyEpicGame from './utils/MyEpicGame.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -81,6 +84,32 @@ const App = () => {
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
+
+  useEffect(() => {
+    const fetchNFTMetadata = async () => {
+      console.log('Checking for Character NFT on address:', currentAccount);
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        MyEpicGame.abi,
+        signer
+      );
+
+      const txn = await gameContract.checkIfUserHasNFT();
+      if (txn.name) {
+        console.log('User has character NFT');
+        setCharacterNFT(transformCharacterData(txn));
+      } else {
+        console.log('No character NFT found');
+      }
+    };
+
+    if (currentAccount) {
+      console.log('CurrentAccount:', currentAccount);
+      fetchNFTMetadata();
+    }
+  }, [currentAccount]);
 
   return (
     <div className="App">
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = '0x8ffd47C66a92E61173f27bd75318C23F2f624c5A';
+
+
+const transformCharacterData = (characterData) => {
+	return {
+		name: characterData.name,
+		imageURI: characterData.imageURI,
+		hp: characterData.hp.toNumber(),
+		maxHp: characterData.maxHp.toNumber(),
+		attackDamage: characterData.attackDamage.toNumeber(),
+	};
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
